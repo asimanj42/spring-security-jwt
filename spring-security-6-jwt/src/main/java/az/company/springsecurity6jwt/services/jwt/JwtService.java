ce690fc461a5aecc49378703e73aae91919db30c
asimanj42 <asimanmemmedli123321@gmail.com>
2023-11-23 00:17:52 +0400

    extracting claims from Jwt and getSignInKey method implemented

--- spring-security-6-jwt/src/main/java/az/company/springsecurity6jwt/services/jwt/JwtService.java
+++ spring-security-6-jwt/src/main/java/az/company/springsecurity6jwt/services/jwt/JwtService.java
@@ -1,10 +1,34 @@
 package az.company.springsecurity6jwt.services.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+    @Value("${application.security.jwt.secret-key}")
+    private  String secretKey;
     public String extractUsername(String token) {
         return null;
     }
+
+    private Claims getClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
 }
