a5949c0120fbd6e7dd905b6765305f391a015648
asimanj42 <asimanmemmedli123321@gmail.com>
2023-11-23 19:51:20 +0400

    Auth model classes added

--- spring-security-6-jwt/src/main/java/az/company/springsecurity6jwt/configs/SecurityConfig.java
+++ spring-security-6-jwt/src/main/java/az/company/springsecurity6jwt/configs/SecurityConfig.java
@@ -1,7 +1,6 @@
-package az.company.springsecurity6jwt.config;
+package az.company.springsecurity6jwt.configs;
 
 import az.company.springsecurity6jwt.filters.JwtAuthenticationFilter;
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ spring-security-6-jwt/src/main/java/az/company/springsecurity6jwt/models/auth/LoginRequest.java
@@ -0,0 +1,16 @@
+package az.company.springsecurity6jwt.models.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+
+    private String email;
+    private String password;
+}
