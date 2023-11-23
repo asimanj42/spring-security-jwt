343df31b198b6566727452b697954fa51aab922e
asimanj42 <asimanmemmedli123321@gmail.com>
2023-11-23 19:03:48 +0400

    UserDetailsService was implemented

---
+++ spring-security-6-jwt/src/main/java/az/company/springsecurity6jwt/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package az.company.springsecurity6jwt.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+}
