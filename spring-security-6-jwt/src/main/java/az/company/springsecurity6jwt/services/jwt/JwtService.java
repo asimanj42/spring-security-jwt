aa2fccf183f694c2d91651ee25907a37558f1f83
asimanj42 <asimanmemmedli123321@gmail.com>
2023-11-23 18:38:13 +0400

    generateToken method implemented

--- spring-security-6-jwt/src/main/java/az/company/springsecurity6jwt/services/jwt/JwtService.java
+++ spring-security-6-jwt/src/main/java/az/company/springsecurity6jwt/services/jwt/JwtService.java
@@ -2,12 +2,17 @@ package az.company.springsecurity6jwt.services.jwt;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -16,6 +21,20 @@ public class JwtService {
     private  String secretKey;
     public String extractUsername(String token) {
         return extractClaim(token,Claims::getSubject);
+    }
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(),userDetails);
+    }
+
+    public String generateToken(Map<String,Object> extraClaims, UserDetails userDetails){
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
     private Claims getClaims(String token){
